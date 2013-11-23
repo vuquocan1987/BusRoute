19bbf59bdc091ef69b4aaf47bc97dbfc8c729858
hnguyen0828 <hnguyen0828@gmail.com>
2013-11-23 10:11:30 +0700

    fix error

--- BusRoute/src/cs/edu/busroute/MainActivity.java
+++ BusRoute/src/cs/edu/busroute/MainActivity.java
@@ -1,5 +1,6 @@
 package cs.edu.busroute;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,7 +14,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		// create our database, should be use
 		busInfoHelper = new BusInfoHelper(this);
 		busInfoHelper.createDatabase();
