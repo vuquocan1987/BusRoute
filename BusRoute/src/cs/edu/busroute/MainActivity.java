5afd2073b9468df9b110e5dce1971a41fa263ba9
hnguyen0828 <hnguyen0828@gmail.com>
2013-11-27 16:14:56 +0700

    Poit to current possition when open map

--- BusRoute/src/cs/edu/busroute/MainActivity.java
+++ BusRoute/src/cs/edu/busroute/MainActivity.java
@@ -1,12 +1,17 @@
 package cs.edu.busroute;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainActivity extends Activity {
 
@@ -43,9 +48,67 @@ public class MainActivity extends Activity {
 	}
 
 	public void openMap(View view) {
-		final Intent mapIntent = new Intent(MainActivity.this,
-				MapActivity.class);
-		startActivity(mapIntent);
+		if (servicesConnected()) {
+			final Intent mapIntent = new Intent(MainActivity.this,
+					MapActivity.class);
+			startActivity(mapIntent);
+		}
+	}
+
+	private boolean servicesConnected() {
+
+		// Check that Google Play services is available
+		int resultCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(this);
+
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// Continue
+			return true;
+			// Google Play services was not available for some reason
+		} else {
+			// Display an error dialog
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode,
+					this, 0);
+			if (dialog != null) {
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(dialog);
+				errorFragment.show(getFragmentManager(), "BusRoute");
+			}
+			return false;
+		}
+	}
+
+	public static class ErrorDialogFragment extends DialogFragment {
+
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+
+		/**
+		 * Default constructor. Sets the dialog field to null
+		 */
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+
+		/**
+		 * Set the dialog to display
+		 * 
+		 * @param dialog
+		 *            An error dialog
+		 */
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+
+		/*
+		 * This method must return a Dialog to the DialogFragment.
+		 */
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
 	}
 
 	@Override
--- BusRoute/src/cs/edu/busroute/MapActivity.java
+++ BusRoute/src/cs/edu/busroute/MapActivity.java
@@ -1,14 +1,26 @@
 package cs.edu.busroute;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements LocationListener,
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
+	private static final int ZOOM_SIZE = 14;
 	private GoogleMap googleMap;
+	private LocationClient mLocationClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,9 @@ public class MapActivity extends Activity {
 			initilizeMap();
 			googleMap.setMyLocationEnabled(true);
 			googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+			googleMap.getUiSettings().setCompassEnabled(true);
+			mLocationClient = new LocationClient(this, this, this);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -42,5 +57,55 @@ public class MapActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		initilizeMap();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart() {
+
+		super.onStart();
+		mLocationClient.connect();
+
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		Location location = mLocationClient.getLastLocation();
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(new LatLng(location.getLatitude(), location
+						.getLongitude())).zoom(ZOOM_SIZE).build();
+
+		googleMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPosition));
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStop() {
+
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			mLocationClient.removeLocationUpdates(this);
+		}
+		// After disconnect() is called, the client is considered "dead".
+		mLocationClient.disconnect();
+
+		super.onStop();
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
